7763cb726418c930760895d9118c8802a8a3ce51
Alexander Sloan <alexander.charles.sloan@gmail.com>
2019-01-30 09:19:42 -0700

    error checking complete

--- Makefile
+++ Makefile
@@ -9,3 +9,9 @@ test: main
 clean:
 	rm -f main
 	rm -f coordsOutput.txt
+
+cover:
+	clang++ -fprofile-instr-generate -fcoverage-mapping main.cpp -o main
+	LLVM_PROFILE_FILE="main.profraw" ./main < coords.txt
+	xcrun llvm-profdata merge -sparse main.profraw -o main.profdata
+	xcrun llvm-cov show ./main -instr-profile=main.profdata
--- main.cpp
+++ main.cpp
@@ -11,8 +11,96 @@
 #include <fstream>
 #include <sstream>
 #include <math.h>
+#include <float.h>
+
+
+
 using namespace std;
 
+// This pair is used to store the X and Y
+// coordinates of a point respectively
+#define pdd pair<double, double>
+
+
+// Function used to display X and Y coordinates
+// of a point
+void displayPoint(pdd P)
+{
+    cout << "(" << P.first << ", " << P.second
+    << ")" << endl;
+}
+
+//https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
+pdd lineLineIntersection(pdd A, pdd B, pdd C, pdd D)
+{
+    // Line AB represented as a1x + b1y = c1
+    double a1 = B.second - A.second;
+    double b1 = A.first - B.first;
+    double c1 = a1*(A.first) + b1*(A.second);
+    
+    // Line CD represented as a2x + b2y = c2
+    double a2 = D.second - C.second;
+    double b2 = C.first - D.first;
+    double c2 = a2*(C.first)+ b2*(C.second);
+    
+    double determinant = a1*b2 - a2*b1;
+    
+    if (determinant == 0)
+    {
+        // The lines are parallel. This is simplified
+        // by returning a pair of FLT_MAX
+        return make_pair(FLT_MAX, FLT_MAX);
+    }
+    else
+    {
+        double x = (b2*c1 - b1*c2)/determinant;
+        double y = (a1*c2 - a2*c1)/determinant;
+        return make_pair(x, y);
+    }
+}
+
+bool doLinesIntersect (const vector<double> &coords){
+    pdd A = make_pair(coords[0], coords[1]);
+    pdd B = make_pair(coords[2], coords[3]);
+    pdd C = make_pair(coords[4], coords[5]);
+    pdd D = make_pair(coords[6], coords[7]);
+    
+    //line AB BC CD DA
+    pdd intersection1 = lineLineIntersection(A, B, C, D);
+    pdd intersection2 = lineLineIntersection(A, D, C, B);
+    
+    int xMax = coords[0];
+    int yMax = coords[1];
+    
+    for(int i=2; i<coords.size(); i+=2){
+        if(coords[i]>xMax){
+            xMax=coords[i];
+        }
+    }
+    
+    for(int i=3; i<coords.size(); i+=2){
+        if(coords[i]>yMax){
+            yMax=coords[i];
+        }
+    }
+    
+//    cout<<intersection1.first<<" "<<intersection1.second<<endl;
+//    cout<<intersection2.first<<" "<<intersection2.second<<endl;
+    if(intersection1.first<xMax && intersection1.second<yMax && intersection1.first>0 && intersection1.second>0){
+
+        //if intersection y > AB max y
+        if(intersection1.second > coords[3]){
+            return false;
+        }
+        return true;
+    }
+    if(intersection2.first<xMax && intersection2.second<yMax && intersection2.second>0 && intersection2.second>0){
+        return true;
+    }
+    
+    return false;
+    //if the intersection is less than both y values
+}
 
 
 bool isParallelogram(const vector<double> &sides, const vector<double> &slopes){
@@ -37,7 +125,7 @@ bool isRectangle(const vector<double> &sides, const vector<double> &slopes){
     if(sides[0]!=sides[2] || sides[1]!=sides[3]) return false;
     if(slopes[0]==0 && slopes[2]==0 && slopes[1]==90 && slopes[3]==90) return true;
     
-
+    
     return false;
 }
 
@@ -77,8 +165,8 @@ vector<double> parseString (const string& input){
     shape.push_back(0);
     shape.push_back(0);
     
-//    istringstream iss(input);
-//    vector<string> results(istream_iterator<string>{iss},istream_iterator<string>());
+    //    istringstream iss(input);
+    //    vector<string> results(istream_iterator<string>{iss},istream_iterator<string>());
     
     vector<string> results;
     
@@ -86,10 +174,18 @@ vector<double> parseString (const string& input){
     
     for(string input; iss>>input;)
         results.push_back(input);
- 
+    
+    
     
     for(int i=0; i<results.size(); i++){
-        shape.push_back(stod(results[i]));
+        //if the string to double conversion is not possible
+        try{
+            shape.push_back(stod(results[i]));
+        }catch(const std::invalid_argument& e){
+            vector<double> fail;
+            return fail;
+        }
+//        shape.push_back(stod(results[i]));
     }
     
     return shape;
@@ -100,7 +196,7 @@ double calculateLength (double x1, double y1, double x2, double y2){
     double distance = 0;
     
     distance = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
-
+    
     return distance;
 }
 
@@ -118,39 +214,45 @@ double calculateSlope(double x1, double y1, double x2, double y2){
     if(dx==0){
         return 90;
     }
-
+    
     double slope = dy / dx;
     
     return slope;
 }
 
-
-
 string determineShape(const vector <double> &coords){
     
     if(coords.size()!=8){
-        cout<<"invalid"<<endl;
-        return "invalid";
+        cout<<"error 1"<<endl;
+        return "error 1";
     }
+    
+    for(int i=0; i<coords.size(); i++){
+        if(coords[i] < 0 || coords[i] > 100){
+            cout<<"error 1"<<endl;
+            return "error 1";
+        }
+    }
+    
     
     //if any 2 points are the same, the input is invalid and cannot form a quadrilateral
     for(int i=0; i<5; i+=2){
         for(int j=i+2; j<coords.size()-1; j+=2){
             if(coords[i]==coords[j]){
                 if(coords[i+1]==coords[j+1]){
-                    cout<<"invalid"<<endl;
-                    return "invalid";
+                    cout<<"error 2"<<endl;
+                    return "error 2";
                 }
             }
         }
     }
     
-    
+
     //a vector to contain the 4 side lengths of the shape
     vector <double> sideLengths;
     //a vector to contain the slopes of the 4 lines
     vector <double> slopes;
-
+    
     sideLengths.push_back(calculateLength(coords[0],coords[1],coords[2],coords[3]));
     sideLengths.push_back(calculateLength(coords[2],coords[3],coords[4],coords[5]));
     sideLengths.push_back(calculateLength(coords[4],coords[5],coords[6],coords[7]));
@@ -161,20 +263,30 @@ string determineShape(const vector <double> &coords){
     slopes.push_back(calculateSlope(coords[4],coords[5],coords[6],coords[7]));
     slopes.push_back(calculateSlope(coords[6],coords[7],coords[0],coords[1]));
     
+    
+    
+    //cout<<result<<endl;
+    
     //if any 3 points form a line, the input does not form a quadrilateral
     if(slopes[0]==slopes[1]){
-        cout<<"invalid"<<endl;
-        return "invalid";
+        cout<<"error 4"<<endl;
+        return "error 4";
     }
     if(slopes[1]==slopes[2]){
-        cout<<"invalid"<<endl;
-        return "invalid";
+        cout<<"error 4"<<endl;
+        return "error 4";
     }
     if(slopes[2]==slopes[3]){
-        cout<<"invalid"<<endl;
-        return "invalid";
+        cout<<"error 4"<<endl;
+        return "error 4";
     }
-
+    
+    if(doLinesIntersect(coords)){
+        cout<<"error 3"<<endl;
+        return "error 3";
+    }
+    
+    
     if(isSquare(sideLengths,slopes)){
         cout<<"square"<<endl;
         return "square";
@@ -183,10 +295,12 @@ string determineShape(const vector <double> &coords){
         cout<<"rectangle"<<endl;
         return "rectangle";
     }
+    
     if(isTrapezoid(slopes)){
         cout<<"trapezoid"<<endl;
         return "trapezoid";
     }
+    
     if(isRhombus(sideLengths,slopes)){
         cout<<"rhombus"<<endl;
         return "rhombus";
@@ -199,9 +313,32 @@ string determineShape(const vector <double> &coords){
         cout<<"kite"<<endl;
         return "kite";
     }
-//    if(isScalene(sideLengths)){
-//        cout<<"scalene"<<endl;
-//        return "scalene";
+    
+    
+    //if point 2 or 3 y value<point 1 y value
+    //and point 2 or 3 x value>point 1 x value
+    //if any lines overlap, throw an error
+//    int pointOneX = coords[2];
+//    int pointOneY = coords[3];
+//
+//    int pointTwoX = coords[4];
+//    int pointTwoY = coords[5];
+//
+//    int pointThreeX = coords[6];
+//    int pointThreeY = coords[7];
+//
+//    if(pointTwoY <= pointOneY){
+//        if(pointTwoX >= pointOneX){
+//            cout<<"error 3"<<endl;
+//            return "error 3";
+//        }
+//    }
+//
+//    if(pointThreeY <= pointOneY){
+//        if(pointThreeX >= pointOneX){
+//            cout<<"error 3"<<endl;
+//            return "error 3";
+//        }
 //    }
     cout<<"quadrilateral"<<endl;
     return "quadrilateral";
@@ -223,11 +360,34 @@ void readInputDetermineShape (){
 
 
 
-
 int main(int argc, const char * argv[]) {
-
+    
     readInputDetermineShape();
+    
+//    pdd A = make_pair(0, 0);
+//    pdd B = make_pair(3, 3);
+//    pdd C = make_pair(1, 8);
+//    pdd D = make_pair(2, 4);
+//
+//    pdd intersection = lineLineIntersection(A, B, C, D);
+//
+//    if (intersection.first == FLT_MAX &&
+//        intersection.second==FLT_MAX)
+//    {
+//        cout << "The given lines AB and CD are parallel.\n";
+//    }
+//
+//    else
+//    {
+//        // NOTE: Further check can be applied in case
+//        // of line segments. Here, we have considered AB
+//        // and CD as lines
+//        cout << "The intersection of the given lines AB "
+//        "and CD is: ";
+//        displayPoint(intersection);
+//    }
     
     
     return 0;
 }
+
